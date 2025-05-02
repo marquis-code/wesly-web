e6545dcf960df0f0dbfa36d2daa8edc4bb3ce276
Marquis <abahmarquis@gmail.com>
2025-05-02 04:35:03 +0100

    generated lock

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,7 @@
 import { defineNuxtConfig } from "nuxt/config";
 
 export default defineNuxtConfig({
+  ssr: false,
   postcss: {
     plugins: {
       "postcss-import": {},
@@ -9,75 +10,9 @@ export default defineNuxtConfig({
       autoprefixer: {},
     },
   },
-  plugins: ["~/plugins/aos.client.ts"],
-  modules: ['@kevinmarrec/nuxt-pwa', "nuxt-emoji-picker"],
-  pwa: {
-    workbox: {
-      enabled: true
-    },
-    meta: {
-      title: "Prayer Streak",
-      author: "Marquis",
-      mobileAppIOS: false,
-      mobileApp: true,
-      description:
-        "Fun, Community, Growth",
-      theme_color: "#27396B",
-      background_color: "#27396B",
-      display: "standalone",
-      start_url: "/",
-      nativeUI: true,
-    },
-    icon: {
-      sizes: [64, 120, 144, 152, 192, 384, 512],
-    },
-    manifest: {
-      name: "Achilles Drill",
-      lang: "fa",
-      useWebmanifestExtension: false,
-    },
-    runtimeCaching: [
-      {
-        urlPattern: "https://fonts.googleapis.com/.*",
-        handler: "cacheFirst",
-        method: "GET",
-        strategyOptions: { cacheableResponse: { statuses: [0, 200] } },
-      },
-      {
-        urlPattern: "https://fonts.gstatic.com/.*",
-        handler: "cacheFirst",
-        method: "GET",
-        strategyOptions: { cacheableResponse: { statuses: [0, 200] } },
-      },
-      {
-        urlPattern: "https://cdn.snipcart.com/.*",
-        method: "GET",
-        strategyOptions: { cacheableResponse: { statuses: [0, 200] } },
-      },
-      {
-        urlPattern:
-          "https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js",
-        handler: "cacheFirst",
-        method: "GET",
-        strategyOptions: { cacheableResponse: { statuses: [0, 200] } },
-      },
-    ],
+  plugins: [],
+  modules: [],
+  build: {
+    transpile: ["lucide-vue-next"],
   }
-  // modules: [
-  //   '@nuxtjs/pwa'
-  // ],
-  // pwa: {
-  //   manifest: {
-  //     name: 'My Awesome Nuxt 3 PWA',
-  //     short_name: 'Nuxt3PWA',
-  //     lang: 'en',
-  //     useWebmanifestExtension: false,
-  //     display: 'standalone',
-  //     theme_color: '#4DBA87',
-  //     background_color: '#000000'
-  //   },
-  //   workbox: {
-  //     // Workbox options for customizing the service worker
-  //   }
-  // }
 });
